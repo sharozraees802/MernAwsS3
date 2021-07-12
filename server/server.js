a6c82e5861f9b70d3c89d30c539cbce1d62d0f78
sharozraees802 <sharozraees802@gmail.com>
2021-07-12 19:27:37 +0500

    credential error in a code1

--- server/server.js
+++ server/server.js
@@ -7,15 +7,15 @@ dotenv.config()
 
 const PORT = process.env.PORT || 5000
 
-// const app = express()
+const app = express()
 
-// app.use(fileupload())
+app.use(fileupload())
 
-// app.use('/api',routes)
+app.use('/api',routes)
 
-// app.listen(PORT,()=>console.log(`🚀 S3 Server is Running http://localhost:${PORT}/`))
-const AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY
-const AWS_SECRET_KEY = process.env.AWS_SECRET_KEY
-const AWS_BUCKET_NAME = process.env.AWS_BUCKET_NAME
+app.listen(PORT,()=>console.log(`🚀 S3 Server is Running http://localhost:${PORT}/`))
+// const AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY
+// const AWS_SECRET_KEY = process.env.AWS_SECRET_KEY
+// const AWS_BUCKET_NAME = process.env.AWS_BUCKET_NAME
 
-console.log(AWS_BUCKET_NAME)
+// console.log(AWS_BUCKET_NAME)
